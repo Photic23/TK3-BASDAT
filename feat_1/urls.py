c3b99d1d9b00733077a8db634bd9b5641c884d45
Amanda Oktivia <112821721+Scarletra@users.noreply.github.com>
2024-04-25 14:06:55 +0700

    add: Added form for adding new playlist

--- feat_1/urls.py
+++ feat_1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from feat_1.views import daftar_playlist_page
+from feat_1.views import daftar_playlist_page, add_playlist
 
 app_name = 'feat_1'
 
 urlpatterns = [
     path('', daftar_playlist_page, name='daftar_playlist_page'),
+    path('add-playlist/', add_playlist, name='add_playlist'),
 ]
--- feat_1/views.py
+++ feat_1/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def daftar_playlist_page(request):
-    context = {}
+    context = {
+        'username': "Scarletra",
+    }
     
     return render(request, 'daftar-playlist.html', context)
+
+def add_playlist(request):
+    context = {
+        'username': "Scarletra",
+    }
+    return render(request, 'form-playlist.html', context)
 
