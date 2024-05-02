0e077b82870d8e2e244e9af44f2869eea1da0e3d
Photic23 <resellcourtesyx@gmail.com>
2024-05-02 21:19:29 +0700

    adding dashboard an fixing regist

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from main.views import show_register
+from main.views import show_register_pegguna, show_dashboard_podcast, show_register_main,show_register_label
 
 app_name = 'main'
 
 urlpatterns = [
-    path('register/', show_register, name='show_register'),
-
+    path('register-user/', show_register_pegguna, name='show_register_pegguna'),
+    path('dashboard-podcast/', show_dashboard_podcast, name='show_dashboard_podcast'),
+    path('register/', show_register_main, name='show_register_main'),
+    path('register-label/', show_register_label, name='show_register_label'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def show_register(request):
+def show_register_main(request):
+    context = {
+    }
+
+    return render(request, "regist_main.html", context)
+
+def show_register_pegguna(request):
     context = {
     }
 
     return render(request, "regist.html", context)
+
+def show_dashboard_podcast(request):
+    context = {
+    }
+
+    return render(request, "dashboard_podcast.html", context)
+
+def show_register_label(request):
+    context = {
+    }
+
+    return render(request, "regist_label.html", context)
