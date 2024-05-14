10f117eec1dca46c1b98c520dd77992d7f6f0ec0
Deacher <reihanea80@gmail.com>
2024-05-14 15:33:31 +0700

    crud - tinggal create & delete

--- cek_royalti/urls.py
+++ cek_royalti/urls.py
@@ -4,5 +4,5 @@ from cek_royalti.views import show_main
 app_name = 'cek-royalti'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('<str:user_email>/', show_main, name='show_main'),
 ]
