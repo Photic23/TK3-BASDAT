e5ce58e234f418eddf4f2b009e5497b99ae216f2
Photic23 <resellcourtesyx@gmail.com>
2024-05-17 16:08:41 +0700

    podcast list for CRUD

--- podcast/views.py
+++ podcast/views.py
@@ -4,6 +4,10 @@ from django.contrib import messages
 from django.views.decorators.http import require_http_methods
 import psycopg2
 from main.connect import get_db_connection
+import uuid
+import random
+from datetime import date
+from datetime import datetime
 
 # Create your views here.
 def show_detail_podcast(request, user_role, id_podcast):
@@ -70,13 +74,60 @@ def show_chart_detail(request):
     return render(request, "chart_detail.html", context)
 
 def create_podcast(request):
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
     context = {
     }
 
     return render(request, "create_podcast.html", context)
 
 def show_podcast_list(request):
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    if request.method == "POST":
+        judul_podcast = request.POST.get("title")
+        genres = request.POST.get("artist")
+        genres_split = genres.split(', ')
+        genres_fix = genres_split[1:]
+        cursor.execute("SELECT id FROM KONTEN")
+        daftar_uuid = cursor.fetchall()
+        cek_uuid = True
+        currentdate = date.today()
+        currentyear = datetime.now().year
+        while cek_uuid:
+            status = True
+            uuid_konten = uuid.uuid4
+            for uuid1 in daftar_uuid:
+                if uuid1 == uuid_konten:
+                    status = False
+            if status == True:
+                cek_uuid = False
+        cursor.execute("INSERT INTO KONTEN VALUES(%s, %s, %s, %s, 0)", (uuid_konten, judul_podcast, currentdate, currentyear,))
+        # cursor.execute("INSERT INTO KONTEN VALUES("+uuid_konten+", "+judul_podcast+", "+currentdate+", "+currentyear+", "+ '0'+")") #INSERT KE KONTEN
+        for genre in genres_fix: #INSERT GENRE
+            cursor.execute("INSERT INTO GENRE VALUES(%s, %s)", (uuid_konten, genre,))
+            # cursor.execute("INSERT INTO GENRE VALUES("+uuid_konten+", "+genre+")")
+        cursor.execute("INSERT INTO PODCAST VALUES(%s, oduboj_ize83@outlook.com)", (uuid_konten,))
+        # cursor.execute("INSERT INTO PODCAST VALUES("+uuid_konten+", oduboj_ize83@outlook.com)")        
+    jumlah_episodes = [] 
+    cursor.execute("SELECT K.id, K.judul, K.durasi FROM KONTEN K WHERE K.id IN(SELECT id_konten FROM PODCAST WHERE email_podcaster = 'oduboj_ize83@outlook.com')") #TODO: DIRUBAH
+    podcasts = cursor.fetchall()
+    for podcast in podcasts:
+        cursor.execute("SELECT COUNT(id_episode) FROM EPISODE WHERE id_konten_podcast=%s", (podcast[0],))
+        jumlah_episodes.append(cursor.fetchone())
+    
+
+    cursor.close()
+    conn.close()
+
+    merged_podcast = [tup1 + tup2 for tup1, tup2 in zip(podcasts, jumlah_episodes)]
+
     context = {
+        'podcasts': merged_podcast,
     }
 
     return render(request, "podcast_list.html", context)
